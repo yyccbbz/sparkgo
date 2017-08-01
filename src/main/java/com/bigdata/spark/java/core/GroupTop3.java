9063177f110b0479480036c7d6be37b8f454720c
yyccbbz <juancan@163.com>
2017-08-02 01:20:53 +0800

    TopN java

--- src/main/java/com/bigdata/spark/java/core/GroupTop3.java
+++ src/main/java/com/bigdata/spark/java/core/GroupTop3.java
@@ -28,32 +28,36 @@ public class GroupTop3 {
 //        JavaRDD<String> lines = sc.textFile("E:\\Workspace\\intellij2017\\sparkgo\\src\\main\\resources\\txt\\score.txt");
         JavaRDD<String> lines = sc.textFile("E:\\Users\\IdeaProjects\\sparkgo\\src\\main\\resources\\txt\\score.txt");
 
-        JavaPairRDD<String, Iterable<Integer>> pairRDD = lines.mapToPair(line ->
+        JavaPairRDD<String, Iterable<Integer>> groupPairs = lines.mapToPair(line ->
                 new Tuple2<String, Integer>(line.split(" ")[0], Integer.parseInt(line.split(" ")[1])))
                 .groupByKey();
 
-        JavaPairRDD<String, Iterable<Integer>> top3 = pairRDD.mapToPair(t -> {
-            Integer[] top3Arr = new Integer[3];
-            Iterator<Integer> scoreIter = t._2.iterator();
-            while (scoreIter.hasNext()) {
-                Integer score = scoreIter.next();
-                for (int i = 0; i < top3Arr.length; i++) {
-                    if (top3Arr[i] == null) {
-                        top3Arr[i] = score;
+        JavaPairRDD<String, Iterable<Integer>> top3Pairs = groupPairs.mapToPair(t -> {
+            Integer[] top3 = new Integer[3];
+            String key = t._1();
+            Iterator<Integer> values = t._2.iterator();
+            while (values.hasNext()) {
+                Integer value = values.next();
+
+                for (int i = 0; i < top3.length; i++) {
+                    if (top3[i] == null) {
+                        top3[i] = value;
                         break;
-                    } else if (score > top3Arr[i]) {
-                        int tmp = top3Arr[i];
-                        top3Arr[i] = score;
-                        if (i < top3Arr.length - 1) {
-                            top3Arr[i + 1] = tmp;
+                    } else if (top3[i] > value) { //索引处的值比value大
+                        // 该位置之后的值向后移动
+                        for (int j = top3.length - 1; j > i; j--) {
+                            top3[j] = top3[j - 1];
                         }
+                        top3[i] = value;
+                        break;
                     }
+                    //否则， //索引处的值比value小， 在top5总继续向后比较
                 }
             }
-            return new Tuple2<String, Iterable<Integer>>(t._1, Arrays.asList(top3Arr));
+            return new Tuple2<String, Iterable<Integer>>(key, Arrays.asList(top3));
         });
 
-        top3.foreach(t -> {
+        top3Pairs.foreach(t -> {
             System.out.println("班级 " + t._1);
             Iterator<Integer> iter = t._2.iterator();
             while (iter.hasNext()) {
